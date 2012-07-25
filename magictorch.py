9c467ea465fac3e9daf9508f7ed90e4f82f0e441
Tom Allen <tom@jugglethis.net>
2012-07-25 14:36:22 +1000

    Commented lk_homography sample from OpenCV2 library. Started changing this to suit a projection model, and implemented an automatic re-keyframing algorithm.

--- magictorch.py
+++ magictorch.py
@@ -1,102 +1,128 @@
-import cv
-import os
-import sys
+'''
+Magic Torch Prototype
+===============================
+Based on the Lucas-Kanade homography tracker from OpenCV samples.
+Lucas-Kanade sparse optical flow demo. Uses goodFeaturesToTrack
+for track initialization and back-tracking for match verification
+between frames. Finds homography between reference and current views.
 
-def GrabAndShow(windowName, captureDevice):
-	frame = cv.QueryFrame(captureDevice)
-	if frame is None:
-		return
-	cv.Flip(frame, None, 1) # Mirror the display.
-	outputImage = cv.CloneImage(frame) # Copy frame to create the base output image.
-	DetectFaces(frame, outputImage)
-	DetectCorners(frame, outputImage)
-#	DetectFeatures(frame, outputImage) # Tom discovered the all important difference between cv and cv2... Will play again tomorrow porting this to cv2.
-	cv.ShowImage(windowName,outputImage)
+Keys
+----
+ESC   - exit
+SPACE - Lock/unlock projection
+r     - toggle RANSAC
+'''
 
-def CreateWindow(windowName):
-	cv.NamedWindow(windowName, flags=cv.CV_WINDOW_AUTOSIZE)
-	cv.MoveWindow(windowName,0,0)
+import numpy as np
+import cv2
+import video
+from common import draw_str # From common.py in this directory.
 
-def CloseWindow(windowName):
-	cv.DestroyWindow(windowName)
+lk_params = dict( winSize  = (19, 19), # Size of search winow at each pyramid level
+maxLevel = 2,
+criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)) # When the alg terminates
 
-def InitCapture():
-	return cv.CaptureFromCAM(-1) # Assume just one camera.
-	#for camera in EnumerateCameras():
-	#	return cv.CaptureFromCAM(camera) # Return first. Todo, GUI selection.
+feature_params = dict( maxCorners = 1000, # Passed to GoodFeaturesToTrack
+qualityLevel = 0.01,
+minDistance = 8,
+blockSize = 19 )
 
-def ReleaseCapture(captureDevice):
-	del captureDevice
+minFeaturesBeforeNewKeyframeRequired = 20 # Automatically grab a new keyframe if less than this many features are viable.
 
-def ConvertToGrayscale(image):
-	imageSize = cv.GetSize(image)
-	
-	# Convert to grayscale
-	grayscale = cv.CreateImage(imageSize, 8, 1)
-	cv.CvtColor(image, grayscale, cv.CV_BGR2GRAY)
-	
-	# Equalise histogram
-	cv.EqualizeHist(grayscale, grayscale)
-	return grayscale
+'''Traces optical flow between two images, then reverses this to assess which features are back-projectable within some tolerance.'''
+def checkedTrace(img0, img1, p0, back_threshold = 1.0):
+	# Calculate flow points (p1) from img0 to img1, using p0 as the baseline
+	p1, st, err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **lk_params)
 
-def DetectFaces(imageIn, imageOut):	
-	grayscale = ConvertToGrayscale(imageIn)
-	
-	# Detect objects
-	cascade = cv.Load('/opt/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml')
-	faces = cv.HaarDetectObjects(grayscale,cascade,cv.CreateMemStorage(0), 1.2, 2, cv.CV_HAAR_DO_CANNY_PRUNING, (50,50))
+	# Calculate flow points (p0r) from img1 to img0, using p1 as the baseline
+	p0r, st, err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **lk_params)
 
-	for ((x,y,w,h),n) in faces:
-		cv.Rectangle(	imageOut, 
-						( int(x), int(y) ),
-						( int(x + w), int(y + h) ),
-						cv.CV_RGB(0, 255, 0), 3, 8, 0
-						)
+	# Calculate the error in back projection for each point
+	d = abs(p0-p0r).reshape(-1, 2).max(-1)
+	status = d < back_threshold # If error < threshold, status is True, else False. This builds an array of statuses.
+	return p1, status # Return the new positions of the features, and the status array
 
-def DetectCorners(imageIn, imageOut):
-	grayscale = ConvertToGrayscale(imageIn)
+green = (0, 255, 0)
+red = (0, 0, 255)
 
-# Harris version
-#	dst = cv.CreateMat(imageIn.height, imageIn.width, cv.CV_32FC1)
-#	blockSize = 3
-#	cv.CornerHarris(grayscale, dst, blocksize )
-#	for i in range(0,dst.width):
-#		for j in range(0,dst.height):
-#			val = cv.Get2D(dst,j,i)
-#			if val[0] > 10e-6:
-#				cv.Circle( imageIn, (i,j), 2, cv.RGB(0,255,0), 1, 8, 0 )
+class App:
+	def __init__(self):
+		self.cam = cv2.VideoCapture(0) # TODO: Enumerate all possible, and select sensibly.
+		self.p0 = None
+		self.use_ransac = True
+		self.frame0 = None
 
-# GoodFeaturesToTrack version
-	cornerCount = 250
-	qualityLevel = 0.1
-	minDistance = 10
-	mask = None
-	blockSize = 3
-	useHarris = 0
-	k = 0.04
-	temp = grayscale # Ignored parameter.
-	corners = cv.GoodFeaturesToTrack(grayscale, temp, temp, cornerCount, qualityLevel, minDistance, mask, blockSize, useHarris, k)
-	
-	for corner in corners:
-		cv.Circle( imageOut, (int(corner[0]),int(corner[1])), 2, cv.RGB(0,255,0), 1, 8, 0 )
+	def run(self):
+		while True:
+			# Read a camera frame, convert to grayscale, and copy.
+			ret, frame = self.cam.read()
+			frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			vis = frame.copy()
 
-def EnumerateCameras():
-	cameraList=[]
-	for i in range(0,256):
-		tempCam = cv.CaptureFromCAM(i)
-		if cv.GetCaptureProperty(tempCam,cv.CV_CAP_PROP_FRAME_WIDTH) != 0.0:
-			cameraList.append(i)
-	return cameraList
-	
-if __name__ == "__main__":
-	print "Press Esc to exit..."
-	windowName = "Test"
-	CreateWindow(windowName)
-	cvCap = InitCapture()
-	while(1):
-		GrabAndShow(windowName, cvCap)
-		key = cv.WaitKey(10)
-		if key == 27: #Esc to quit.
-			break
-	CloseWindow(windowName)
-	ReleaseCapture(cvCap)
+			# If we have a prior set of features to track...
+			if self.p0 is not None:
+				# Get the positions (p2) of the features (p1) in the new frame, and their statuses.
+				p2, trace_status = checkedTrace(self.gray1, frame_gray, self.p1)
+
+				# Update variables for next iteration, saving only the good features.
+				self.p1 = p2[trace_status].copy()
+				self.p0 = self.p0[trace_status].copy()
+				self.gray1 = frame_gray
+
+				# If there are less than minFeaturesBeforeNewKeyframeRequired features, tracking is impossible, so clear the baseline features (p0)
+				if len(self.p0) < minFeaturesBeforeNewKeyframeRequired:
+					self.frame0 = frame.copy() # Save the original frame
+					self.p0 = cv2.goodFeaturesToTrack(frame_gray, **feature_params)
+					if self.p0 is not None:
+						self.p1 = self.p0
+						self.gray0 = frame_gray
+						self.gray1 = frame_gray
+						continue
+
+				# Find the perspective transformation between the sets of features.
+				H, status = cv2.findHomography(self.p0, self.p1, (0, cv2.RANSAC)[self.use_ransac], 10.0)
+				h, w = frame.shape[:2] # Get height and width of camera frame.
+				overlay = cv2.warpPerspective(self.frame0, H, (w, h)) # Warp the original frame by this homography matrix
+				vis = cv2.addWeighted(vis, 0.5, overlay, 0.5, 0.0) # and draw translucently over the current frame (this has the effect that the original image looks bright, within the dull-by-comparison live feed).
+
+				for (x0, y0), (x1, y1), good in zip(self.p0[:,0], self.p1[:,0], status[:,0]):
+					if good:
+						cv2.line(vis, (x0, y0), (x1, y1), (0, 128, 0)) # Draw a green line from the original feature to its new location.
+						cv2.circle(vis, (x1, y1), 2, (red, green)[good], -1) # Draw a green circle on the new location of good features, and red for bad.
+						draw_str(vis, (20, 20), 'track count: %d' % len(self.p1)) # Draw an overlay detailing the amount of tracks.
+						if self.use_ransac:
+							draw_str(vis, (20, 40), 'RANSAC')
+
+			# Else, if no prior set of features to track...
+			else:
+				# Find some features in the grayscale image.
+				p = cv2.goodFeaturesToTrack(frame_gray, **feature_params)
+				if p is not None:
+					# Draw them to the frame copy.
+					for x, y in p[:,0]:
+						cv2.circle(vis, (x, y), 2, green, -1)
+						draw_str(vis, (20, 20), 'feature count: %d' % len(p))
+
+			# Display the frame copy (with annotations)
+			cv2.imshow('MagicTorch', vis)
+
+			ch = 0xFF & cv2.waitKey(1)
+			if ch == 27: # If Esc, exit
+				break
+			if ch == ord(' ') or self.frame0 is None: # If space, use the current frame's features as the baseline
+				self.frame0 = frame.copy() # Save the original frame
+				self.p0 = cv2.goodFeaturesToTrack(frame_gray, **feature_params)
+				if self.p0 is not None:
+					self.p1 = self.p0
+					self.gray0 = frame_gray
+					self.gray1 = frame_gray
+			if ch == ord('r'): # If r, toggle whether to use ransac.
+				self.use_ransac = not self.use_ransac
+
+def main():
+	print __doc__
+	App().run()
+	cv2.destroyAllWindows() 			
+
+if __name__ == '__main__':
+	main()
